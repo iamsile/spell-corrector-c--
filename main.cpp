515ea4e1980e5193fcc9438380de0fa5d470d218
sile <iamsai007@sina.com>
2013-11-02 15:05:03 +0800

    spell corrector --c++

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  spell corrector
+//
+//  Created by taowei on 13-11-2.
+//  Copyright (c) 2013年 taowei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
